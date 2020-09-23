5d86a33b0921183ba4115278e7898b90aa1baf94
mcpca <mca@netc.eu>
2020-09-23 17:23:04 +0200

    KTH/AlgaeBloom/Simulators/Density: Added task.

---
+++ src/KTH/AlgaeBloom/Simulators/Density/Density.hpp
@@ -0,0 +1,126 @@
+//***************************************************************************
+// Copyright 2020 KTH Royal Institute of Technology                         *
+//***************************************************************************
+// Author: Miguel Aguiar (aguiar@kth.se)                                    *
+//***************************************************************************
+
+#include <algorithm>
+#include <memory>
+#include <stdexcept>
+
+#include <DUNE/Tasks/Task.hpp>
+#include <DUNE/Parsers/HDF5Reader.hpp>
+#include <DUNE/Math/Grid.hpp>
+
+namespace KTH
+{
+  //! Insert short task description here.
+  //!
+  //! Insert explanation on task behaviour here.
+  //! @author Miguel Aguiar
+  namespace AlgaeBloom
+  {
+    namespace Simulators
+    {
+      namespace Density
+      {
+        inline double
+        interpolateLinear2d(double const* values, double const* delta)
+        {
+          return (1 - delta[0]) * (1 - delta[1]) * values[0]
+                 + delta[0] * (1 - delta[1]) * values[1]
+                 + delta[0] * delta[1] * values[2]
+                 + (1 - delta[0]) * delta[1] * values[3];
+        }
+
+        enum Dimensions
+        {
+          DIM_TIME = 0,
+
+          // DIM_DEPTH must always equal 1.
+          DIM_DEPTH,
+
+          DIM_LAT,
+          DIM_LON
+        };
+
+        class DensityField
+        {
+        public:
+          DensityField(std::string const& path, std::string const& dataset,
+                       std::size_t time_slice, double default_val,
+                       DUNE::Tasks::Task* owner)
+          : m_default(default_val), m_owner(owner)
+          {
+            DUNE::Parsers::HDF5Reader f(path);
+
+            auto data = f.getDataset<double>(dataset);
+            m_dims = std::move(data.dimensions);
+            auto field_values = std::move(data.data);
+
+            std::size_t const space_dims = m_dims[DIM_LAT] * m_dims[DIM_LON];
+
+            if ((time_slice + 1u) * (space_dims) > field_values.size())
+              throw std::runtime_error("Bad dataset dimension");
+
+            m_data = std::vector<double>{
+              std::begin(field_values) + time_slice * space_dims,
+              std::begin(field_values) + (time_slice + 1) * space_dims
+            };
+
+            m_lon = f.getDataset<double>("longitude").data;
+            m_lat = f.getDataset<double>("latitude").data;
+
+            std::vector<double> min = { m_lat[0], m_lon[0] };
+            std::vector<double> max = { m_lat.back(), m_lon.back() };
+            std::vector<size_t> dims = { m_dims[DIM_LAT], m_dims[DIM_LON] };
+
+            m_grid = std::make_unique<DUNE::Math::Grid<2>>(min, max, dims);
+          }
+
+          float
+          evaluate(double lat, double lon)
+          try
+          {
+            auto neighbor = m_grid->getCorner({ lat, lon });
+
+            auto corner = m_grid->getCoordinates(neighbor);
+
+            double delta[2] = { (lat - corner[0]) / m_grid->getSpacing(0),
+                                (lon - corner[1]) / m_grid->getSpacing(1) };
+
+            double vals[4];
+
+            vals[0] = m_data[m_grid->getOffset(neighbor)];
+
+            neighbor[0] += 1;
+            vals[1] = m_data[m_grid->getOffset(neighbor)];
+
+            neighbor[1] += 1;
+            vals[2] = m_data[m_grid->getOffset(neighbor)];
+
+            neighbor[0] -= 1;
+            vals[3] = m_data[m_grid->getOffset(neighbor)];
+
+            return interpolateLinear2d(vals, delta);
+          }
+          catch (std::exception const& e)
+          {
+            m_owner->spew("Error getting field value: %s", e.what());
+            return m_default;
+          }
+
+        private:
+          std::vector<double> m_data;
+          std::vector<std::size_t> m_dims;
+          std::vector<double> m_lat;
+          std::vector<double> m_lon;
+          std::unique_ptr<DUNE::Math::Grid<2>> m_grid;
+
+          double m_default;
+          DUNE::Tasks::Task* m_owner;
+        };
+      } // namespace Density
+    }   // namespace Simulators
+  }     // namespace AlgaeBloom
+} // namespace KTH
---
+++ src/KTH/AlgaeBloom/Simulators/Density/Task.cpp
@@ -0,0 +1,115 @@
+//***************************************************************************
+// Copyright 2020 KTH Royal Institute of Technology                         *
+//***************************************************************************
+// Author: Miguel Aguiar (aguiar@kth.se)                                    *
+//***************************************************************************
+
+// DUNE headers.
+#include <DUNE/DUNE.hpp>
+
+// Local headers.
+#include "Density.hpp"
+
+namespace KTH
+{
+  //! Insert short task description here.
+  //!
+  //! Insert explanation on task behaviour here.
+  //! @author Miguel Aguiar
+  namespace AlgaeBloom
+  {
+    namespace Simulators
+    {
+      namespace Density
+      {
+        using DUNE_NAMESPACES;
+
+        struct Arguments
+        {
+          std::string path;
+          std::string dataset_name;
+          unsigned time_slice;
+          float default_value;
+        };
+
+        struct Task : public DUNE::Tasks::Task
+        {
+          Arguments m_args;
+          DensityField* m_field;
+
+          //! Constructor.
+          //! @param[in] name task name.
+          //! @param[in] ctx context.
+          Task(const std::string& name, Tasks::Context& ctx)
+          : DUNE::Tasks::Task(name, ctx), m_field(NULL)
+          {
+            bind<IMC::EstimatedState>(this);
+
+            param("Data File Path", m_args.path);
+            param("Dataset Name", m_args.dataset_name);
+            param("Time Slice", m_args.time_slice).defaultValue("0");
+            param("Default Value", m_args.default_value).defaultValue("0.0");
+          }
+
+          void
+          consume(IMC::EstimatedState const* es)
+          {
+            // Don't consume other vehicles' states.
+            if (es->getSource() != getSystemId())
+            {
+              spew("Ignoring ES msg from %u (I\'m %u)", es->getSource(),
+                   getSystemId());
+              return;
+            }
+
+            // Get vehicle pos
+            double lat = es->lat;
+            double lon = es->lon;
+            WGS84::displace(es->x, es->y, &lat, &lon);
+
+            IMC::Chlorophyll msg;
+
+            msg.value
+            = m_field ? m_field->evaluate(lat, lon) : m_args.default_value;
+
+            dispatch(msg);
+          }
+
+          //! Acquire resources.
+          void
+          onResourceAcquisition(void)
+          try
+          {
+            m_field
+            = new DensityField(m_args.path, m_args.dataset_name,
+                               m_args.time_slice, m_args.default_value, this);
+          }
+          catch (std::exception const& e)
+          {
+            err("Could not create DensityField: %s", e.what());
+            m_field = NULL;
+          }
+
+          //! Release resources.
+          void
+          onResourceRelease(void)
+          {
+            Memory::clear(m_field);
+          }
+
+          //! Main loop.
+          void
+          onMain(void)
+          {
+            while (!stopping())
+            {
+              waitForMessages(1.0);
+            }
+          }
+        };
+      } // namespace Density
+    }   // namespace Simulators
+  }     // namespace AlgaeBloom
+} // namespace KTH
+
+DUNE_TASK
