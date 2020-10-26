a4bad2853eed2ee810a8d10630286c2cb43dd38f
mcpca <mca@netc.eu>
2020-10-26 10:13:32 +0100

    KTH/AlgaeBloom/Control/ChlGuidance: Implement bearing controller.

--- src/KTH/AlgaeBloom/Control/ChlGuidance/Task.cpp
+++ src/KTH/AlgaeBloom/Control/ChlGuidance/Task.cpp
@@ -53,6 +53,10 @@ namespace KTH
           float angle;
           // Horizontal distance travelled by the vehicle.
           float distance;
+          // Front seeking gain
+          float seeking_gain;
+          // Front following gain
+          float following_gain;
         };
 
         struct Sample
@@ -64,25 +68,20 @@ namespace KTH
 
         struct Arguments
         {
-          unsigned n_ma_pts;
           float initial_speed;
           float initial_heading;
           float target_value;
-          float along_track_gain;
-          float cross_track_gain;
         };
 
         struct Task : public DUNE::Tasks::Task
         {
           Arguments m_args;
           double m_last_data_timestamp;
-          std::unique_ptr<Math::MovingAverage<float>> m_data;
+
+          std::vector<Sample> m_samples;
 
           ControllerState m_state;
           ControllerParameters m_params;
-
-          float m_chl_val = 0.0;
-          float m_chl_grad = 0.0;
 
           bool m_inited = false;
           bool m_wpt_cleared = true;
@@ -110,13 +109,10 @@ namespace KTH
 
             param("Target Chl Value", m_args.target_value).defaultValue("0.5");
 
-            param("Along Track Gain", m_args.along_track_gain)
-            .defaultValue("1.0");
+            param("Following Gain", m_params.following_gain)
+            .defaultValue("3.0");
 
-            param("Cross Track Gain", m_args.cross_track_gain)
-            .defaultValue("1.0");
-
-            param("Moving Average Size", m_args.n_ma_pts).defaultValue("60");
+            param("Seeking Gain", m_params.seeking_gain).defaultValue("1.0");
 
             param("Zigzag Angle", m_params.angle)
             .defaultValue("45")
@@ -132,31 +128,14 @@ namespace KTH
           }
 
           void
-          onResourceAcquisition(void)
-          {
-            m_data
-            = std::make_unique<Math::MovingAverage<float>>(m_args.n_ma_pts);
-          };
-
-          void
           consume(IMC::Chlorophyll const* msg)
           {
-            float new_val = m_data->update(msg->value);
+            double lat = m_state.vp.lat;
+            double lon = m_state.vp.lon;
 
-            if (m_data->sampleSize() != m_args.n_ma_pts)
-              return;
+            WGS84::displace(m_state.rp.x, m_state.rp.y, &lat, &lon);
 
-            float old_time = m_last_data_timestamp;
-            m_last_data_timestamp = Clock::get();
-
-            m_chl_grad
-            = (new_val - m_chl_val) / (m_last_data_timestamp - old_time);
-
-            spew("%.8f %.8f %.12f", m_chl_val, new_val, m_chl_grad);
-
-            m_chl_val = new_val;
-
-            m_data->clear();
+            m_samples.push_back({ lat, lon, msg->value });
           }
 
           void
@@ -169,6 +148,9 @@ namespace KTH
             double lon;
 
             Coordinates::toWGS84(*msg, lat, lon);
+
+            if (lat < 0.01 || lon < 0.01)
+              return;
 
             if (!m_inited)
             {
@@ -241,12 +223,10 @@ namespace KTH
 
               updateDirection();
 
-              resetVirtualPos();
+              // resetVirtualPos();
             }
-            else
-            {
-              updateVirtualPos();
-            }
+
+            updateVirtualPos();
 
             m_prev_ref = m_ref;
             updateRef();
@@ -284,7 +264,61 @@ namespace KTH
           void
           updateDirection(void)
           {
+            Matrix regressor(m_samples.size(), 3);
+            Matrix outputs(m_samples.size(), 1);
 
+            for (unsigned row = 0; row < m_samples.size(); ++row)
+            {
+              regressor(row, 0) = 1.0;
+              regressor(row, 1) = m_samples[row].lat;
+              regressor(row, 2) = m_samples[row].lon;
+
+              outputs(row) = m_samples[row].chl_value;
+            }
+
+            Matrix regressor_t = transpose(regressor);
+            Matrix parameters
+            = inverse(regressor_t * regressor) * regressor_t * outputs;
+
+            inf("Gradient: %.6f %.6f", parameters(1), parameters(2));
+
+            // Deal with zero gradient
+            Matrix psi(2, 1);
+
+            psi(0) = parameters(1);
+            psi(1) = parameters(2);
+
+            double const norm = psi.norm_2();
+
+            if (norm < 1e-6)
+            {
+              war("Small gradient signal, using default heading");
+              m_state.direction = m_args.initial_heading;
+              return;
+            }
+
+            psi = psi / psi.norm_2();
+
+            Matrix epsi(2, 1);
+
+            // Direction orthogonal to the gradient
+            epsi(0) = -psi(1);
+            epsi(1) = psi(0);
+
+            double const error
+            = (m_samples.back().chl_value - m_args.target_value);
+            Matrix const u_seek = -m_params.seeking_gain * error * psi;
+            Matrix const u_follow = m_params.following_gain * epsi;
+            Matrix const u = u_seek + u_follow;
+
+            spew("seek control: %.4f, %.4f", u_seek(0), u_seek(1));
+            spew("follow control: %.4f, %.4f", u_follow(0), u_follow(1));
+
+            spew("Chl error: %.4f", error);
+
+            double const heading = std::atan2(u(1), u(0));
+            spew("Heading: %.2f", Angles::degrees(heading));
+            m_state.direction = heading;
           }
 
           void
